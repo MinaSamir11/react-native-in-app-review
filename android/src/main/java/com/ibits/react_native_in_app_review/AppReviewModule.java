04da1596c74a8057bc0274f2680b54dcb34d38ee
MinaSamir11 <menasamer11@gmail.com>
2020-11-24 04:58:35 -0500

    Solve bug app crash when play store not installed in user device

--- android/src/main/java/com/ibits/react_native_in_app_review/AppReviewModule.java
+++ android/src/main/java/com/ibits/react_native_in_app_review/AppReviewModule.java
@@ -1,7 +1,7 @@
 package com.ibits.react_native_in_app_review;
 
-import android.widget.Toast;
-
+import android.content.Context;
+import android.text.TextUtils;
 import androidx.annotation.NonNull;
 import android.util.Log;
 
@@ -27,27 +27,44 @@ public class AppReviewModule extends ReactContextBaseJavaModule {
     
     @ReactMethod
     public void show() {
-        ReviewManager manager = ReviewManagerFactory.create(getReactApplicationContext());
-        Task<ReviewInfo> request = manager.requestReviewFlow();
+        if(isPlayStoreInstalled(getReactApplicationContext())) {
+            ReviewManager manager = ReviewManagerFactory.create(getReactApplicationContext());
+            Task<ReviewInfo> request = manager.requestReviewFlow();
+            Log.e("isPlayStoreInstalled",isPlayStoreInstalled(getReactApplicationContext())+"");
 
-        request.addOnCompleteListener(task -> {
-             if (task.isSuccessful()) {
-                // We can get the ReviewInfo object
-                ReviewInfo reviewInfo = task.getResult();
-                Task<Void> flow = manager.launchReviewFlow(getCurrentActivity(), reviewInfo);
+            request.addOnCompleteListener(task -> {
+                if (task.isSuccessful()) {
+                    // We can get the ReviewInfo object
+                    ReviewInfo reviewInfo = task.getResult();
+                    Task<Void> flow = manager.launchReviewFlow(getCurrentActivity(), reviewInfo);
 
-                flow.addOnCompleteListener(taski -> {
-                    // The flow has finished. The API does not indicate whether the user
-                    // reviewed or not, or even whether the review dialog was shown. Thus, no
-                    // matter the result, we continue our app flow.
-                    Log.e("Review isSuccessful",""+taski.isSuccessful());
-                });
+                    flow.addOnCompleteListener(taski -> {
+                        // The flow has finished. The API does not indicate whether the user
+                        // reviewed or not, or even whether the review dialog was shown. Thus, no
+                        // matter the result, we continue our app flow.
+                        Log.e("Review isSuccessful", "" + taski.isSuccessful());
+                    });
 
-            } else {
-                Log.e("Review Error",task.getResult().toString());
-            }
+                } else {
+                    Log.e("Review Error", task.getResult().toString());
+                }
 
-        });
+            });
+        }else{
+            Log.e("isPlayStoreInstalled",isPlayStoreInstalled(getReactApplicationContext())+"");
+        }
     }
 
+    public static boolean isPlayStoreInstalled(Context context) {
+        boolean result = false;
+        String playStorePackageName = "com.android.vending";
+        
+        try {
+            String installer = context.getPackageManager()
+                    .getInstallerPackageName(playStorePackageName);
+            result = !TextUtils.isEmpty(installer);
+        } catch (Throwable e) {
+        }
+        return result;
+    }
 }
