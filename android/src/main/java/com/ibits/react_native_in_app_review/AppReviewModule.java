2a50bacbe6a0ba6490455671ba368e618dd336dd
MinaSamir11 <menasamer11@gmail.com>
2021-01-21 09:41:21 +0200

    WIP: add promise when calling func requestAppReview to track flow behaviour if error happen (android).

--- android/src/main/java/com/ibits/react_native_in_app_review/AppReviewModule.java
+++ android/src/main/java/com/ibits/react_native_in_app_review/AppReviewModule.java
@@ -1,28 +1,25 @@
 package com.ibits.react_native_in_app_review;
 
-import android.content.Context;
-import android.content.pm.ApplicationInfo;
-import android.content.pm.PackageManager;
-import android.text.TextUtils;
 import androidx.annotation.NonNull;
 import android.util.Log;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.Promise;
 import com.google.android.play.core.review.ReviewInfo;
 import com.google.android.play.core.review.ReviewManager;
 import com.google.android.play.core.review.ReviewManagerFactory;
 import com.google.android.play.core.tasks.Task;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
-import com.google.android.gms.common.GooglePlayServicesUtil;
 
-import java.util.List;
+import java.util.Objects;
 
 public class AppReviewModule extends ReactContextBaseJavaModule {
 
     private ReactApplicationContext mContext;
+    private Promise pendingPromise;
 
     public AppReviewModule(ReactApplicationContext reactContext) {
         super(reactContext);
@@ -36,45 +33,55 @@ public class AppReviewModule extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public void show() {
+    public void show(final Promise promise) {
+        this.pendingPromise = promise;
+
         if(isGooglePlayServicesAvailable()) {
             ReviewManager manager = ReviewManagerFactory.create(mContext);
             Task<ReviewInfo> request = manager.requestReviewFlow();
-            Log.e("isGooglePlayServicesAvailable",isGooglePlayServicesAvailable()+"");
+            Log.e("isGooglePlaySerAvail.",isGooglePlayServicesAvailable()+"");
 
             request.addOnCompleteListener(task -> {
                 if (task.isSuccessful()) {
                     // We can get the ReviewInfo object
-                    try {
-                        ReviewInfo reviewInfo = task.getResult();
-                        Task<Void> flow = manager.launchReviewFlow(getCurrentActivity(), reviewInfo);
-
-                        flow.addOnCompleteListener(taski -> {
+                    ReviewInfo reviewInfo = task.getResult();
+                    Task<Void> flow = manager.launchReviewFlow(Objects.requireNonNull(getCurrentActivity()), reviewInfo);
+                    flow.addOnCompleteListener(reviewFlow -> {
                             // The flow has finished. The API does not indicate whether the user
                             // reviewed or not, or even whether the review dialog was shown. Thus, no
                             // matter the result, we continue our app flow.
-                            Log.e("Review isSuccessful", "" + taski.isSuccessful());
-                        });
-                    } catch (Exception e) {
-                        Log.e("Review Error", "getResult may have thrown an exception. This is likely an emulated device.");
-                    }
+                        if(reviewFlow.isSuccessful()) {
+                            resolvePromise(reviewFlow.isSuccessful());
+                        }else{
+                            resolvePromise(false);
+                        }
+                    });
                 } else {
-                    String taskErrorMessage = "";
-                    try {
-                        taskErrorMessage = task.getResult().toString();
-                    } catch (Exception e) {
-                        taskErrorMessage = e.getMessage();
-                    }
-                    Log.e("Review Error", taskErrorMessage);
+                    // There was some problem, continue regardless of the result.
+                    rejectPromise("FAILED_REQUEST_REVIEW_FLOW",new Error(Objects.requireNonNull(task.getException()).getMessage()));
                 }
-
             });
         }else{
-            Log.e("isGooglePlayServicesAvailable",isGooglePlayServicesAvailable()+"");
+            Log.e("isGooglePlaySerAvail.",isGooglePlayServicesAvailable()+"");
+            rejectPromise("ERROR_GOOGLE_SERVICES",new Error("GOOGLE_SERVICES_NOT_AVAILABLE"));
         }
     }
 
-    public boolean isGooglePlayServicesAvailable() {
+    private void rejectPromise(String code, Error err) {
+        if (this.pendingPromise != null) {
+            this.pendingPromise.reject(code, err);
+            this.pendingPromise = null;
+        }
+    }
+
+    private void resolvePromise(boolean hasFlowFinishedSuccessfully) {
+        if (this.pendingPromise != null) {
+            this.pendingPromise.resolve(hasFlowFinishedSuccessfully);
+            this.pendingPromise = null;
+        }
+    }
+
+    private boolean isGooglePlayServicesAvailable() {
         GoogleApiAvailability GMS = GoogleApiAvailability.getInstance();
         int isGMS = GMS.isGooglePlayServicesAvailable(mContext);
         return isGMS == ConnectionResult.SUCCESS;
