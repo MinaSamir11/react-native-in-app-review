0ddb13582ba27b33048164adb3481eb10fb2a9b5
yuu383 <yuukikokubo.pg@gmail.com>
2021-02-23 05:24:27 +0900

    Fix the return type of RequestInAppReview

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 declare module 'react-native-in-app-review';
 
-export function RequestInAppReview(): Promise<void>;
+export function RequestInAppReview(): Promise<boolean>;
 
 export function isAvailable(): boolean;
